1df305c15cdefa08207eccdf14b2e5a2d024b3c0
shad <shadislam999@gmail.com>
2024-02-14 23:32:12 +0600

    Text update

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,78 @@ class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       
-      home: Scaffold(
+      home: StoryPage(),
+        );
+        
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
         appBar: AppBar(
 
             title: Center(
               child:Text(
                 'Destini App'
               ) ,),
-        )
         ),
-        );
+
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 50.0,horizontal: 15.0),
+              constraints: BoxConstraints.expand(),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+              children:<Widget> [
+               
+
+                   Expanded(
+                    child: Center(
+                      child: Text(
+                        'here the story pages goes',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                        ),
+                        ),
+                        
+                        
+                        ),
+                    
+                    ),
+               Expanded(
+                child: TextButton(
+                  child: Text('i will do it'),
+                  onPressed: () {
+                    
+                  },
+                  ),
+                ),
+                
+                 Expanded(
+                child: TextButton(
+                  child: Text('i will never do it'),
+                  onPressed: () {
+                    
+                  },
+                  ),
+                ),
+                
+                 
+               
+              ],
+              ),
+            ),
         
+        );
   }
 }
